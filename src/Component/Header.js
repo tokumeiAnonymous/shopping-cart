48d5ce7c6634ec7b0435d042ae5cc857d6932bdb
jerome <jerometagubanoone@gmail.com>
2022-05-19 20:05:48 +0800

    Updated header to be responsive

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -6,24 +6,24 @@ import logoSrc from '../Assets/Shopp-e.png';
 
 export default function Header() {
     return (
-        <header className='header'>
+        <header className="header">
             {/* Routing for the links neglect warning for now*/}
             <a href="#" className="header--logo">
                 <img src={logoSrc}/>
             </a>
-            <div className="header--title">SHOPP - E</div>
-            <ul>
+            <div className="header--title">SHOPP-E</div>
+            <ul className="header--nav">
                 <li>
-                    <a href="#"><img src={homeSrc} alt="Home" /></a>
+                    <a id="header--home" href="#"><img src={homeSrc} alt="Home" /></a>
                 </li>
                 <li>
-                    <a href="#"><img src={shopSrc} alt="Shop" /></a>
+                    <a id="header--shop" href="#"><img src={shopSrc} alt="Shop" /></a>
                 </li>
                 <li>
-                    <a href="#"><img src={cartSrc} alt="Cart" /></a>
+                    <a id="header--cart" href="#"><img src={cartSrc} alt="Cart" /></a>
                 </li>
                 <li>
-                    <a href="#"><img src={accountSrc} alt="Account" /></a>
+                    <a id="header--account" href="#"><img src={accountSrc} alt="Account" /></a>
                 </li>
             </ul>
         </header>
