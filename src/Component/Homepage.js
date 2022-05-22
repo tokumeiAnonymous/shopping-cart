43c97891025b2459ebe8f531134ade6c1e0e2feb
jerome <jerometagubanoone@gmail.com>
2022-05-22 23:09:28 +0800

    Update homepage to be responsive

--- src/Component/Homepage.js
+++ src/Component/Homepage.js
@@ -7,11 +7,13 @@ import { NavLink } from 'react-router-dom';
 export default function Homepage() {
     return (
         <main className='home'>
-            <img id='home--fashion' src={fashionSale} alt=' Fashion Sale ' />
-            <NavLink to='/shop' className='home--shop--link'> 
-                Shop now!
-            </NavLink>
-            <div>
+            <div className='home--first'>
+                <img id='home--fashion' src={fashionSale} alt=' Fashion Sale ' />
+                <NavLink to='/shop' className='home--shop--link'>
+                    Shop now!
+                </NavLink>
+            </div>
+            <div className='home--second'>
                 <img id='home--flash' src={flashSale} alt=' Flash Sale ' />
                 <img id='home--summer' src={summerSale} alt=' Summer Sale ' />
                 <img id='home--voucher' src={voucher} alt=' Voucher Code ' />
