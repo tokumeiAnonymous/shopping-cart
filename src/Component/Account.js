f6213b47457f44bfaf260aa2fed8ade061d9fe83
jerome <jerometagubanoone@gmail.com>
2022-06-02 21:27:56 +0800

    Added account style

--- src/Component/Account.js
+++ src/Component/Account.js
@@ -1,8 +1,12 @@
+import './Account.css';
+
 export default function Account({balance}) {
     return (
         <main className='account'>
-            <div className="profile">John Doe</div>
-            <div>Current balance: ${balance}</div>
+            <div className="account--details">
+                <div className="profile">John Doe</div>
+                <div>Current balance: ${balance}</div>
+            </div>
         </main>
     )
 }
